6275ed3ddc8f33fc692be8e2943edd191f9551d2
Aleksandr Soloshenko <capcom@soft-c.ru>
2024-09-13 18:00:47 +0700

    [db] add TypeORM

---
+++ src/db/index.ts
@@ -0,0 +1,20 @@
+import { join } from "path";
+import { DataSource, DataSourceOptions } from "typeorm";
+
+export const dataSourceOptions = {
+    type: 'mysql',
+    host: process.env.DB_HOST || 'localhost',
+    port: +(process.env.DB_PORT || 3306),
+    username: process.env.DB_USERNAME || 'car-qr-link/landing',
+    password: process.env.DB_PASSWORD || 'car-qr-link/landing',
+    database: process.env.DB_NAME || 'car-qr-link/landing',
+    entities: join(__dirname, '../**/*.entity{.ts,.js}'),
+    synchronize: process.env.NODE_ENV !== 'production',
+
+    migrations: join(__dirname, '../migrations/*{.ts,.js}'),
+    migrationsRun: process.env.NODE_ENV === 'production',
+};
+
+const dataSource = new DataSource(dataSourceOptions as unknown as DataSourceOptions);
+
+export default dataSource;
