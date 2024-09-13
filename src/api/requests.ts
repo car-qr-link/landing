f87a7151427735ba1b8d8218f616e2f0991da415
Aleksandr Soloshenko <capcom@soft-c.ru>
2024-09-13 17:49:44 +0700

    [refactor] migrate to Nest

--- src/api/requests.ts
+++
@@ -1,7 +0,0 @@
-import { z } from "zod";
-
-export const SubscribeRequestSchema = z.object({
-    email: z.string().email(),
-});
-
-export type SubscribeRequest = z.infer<typeof SubscribeRequestSchema>;
---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { Logger } from 'nestjs-pino';
+import { join } from 'path';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    bufferLogs: true,
+  });
+  app.disable('x-powered-by');
+  app.set('trust proxy', true);
+
+  app.useLogger(app.get(Logger));
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('ejs');
+
+  await app.listen(3000);
+}
+bootstrap();
