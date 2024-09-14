de3cd3aa5b38abf361913b67dad44242318a2dad
Aleksandr Soloshenko <capcom2me@gmail.com>
2024-09-14 11:03:29 +0700

    [subscribe] add subscribe request

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { LoggerModule } from 'nestjs-pino';
+import { DataSourceOptions } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { LoggerModule } from 'nestjs-pino';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { SubscriptionsModule } from './core/subscriptions/subscriptions.module';
 import { dataSourceOptions } from './db';
-import { DataSourceOptions } from 'typeorm';
 
 @Module({
   imports: [
@@ -19,8 +20,24 @@ import { DataSourceOptions } from 'typeorm';
             : undefined,
       },
     }),
-    ConfigModule,
-    TypeOrmModule.forRoot(dataSourceOptions as unknown as DataSourceOptions),
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (_: ConfigService) => ({
+        ...dataSourceOptions,
+        type: 'mysql',
+        host: process.env.DB_HOST || 'localhost',
+        port: +(process.env.DB_PORT || 3306),
+        username: process.env.DB_USERNAME || 'car-qr-link/landing',
+        password: process.env.DB_PASSWORD || 'car-qr-link/landing',
+        database: process.env.DB_NAME || 'car-qr-link/landing',
+        synchronize: process.env.NODE_ENV !== 'production',
+
+        migrationsRun: process.env.NODE_ENV === 'production',
+      } as unknown as DataSourceOptions)
+    }),
+    SubscriptionsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/core/subscriptions/subscriptions.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Subscription } from './entities/subscription.entity';
+
+@Injectable()
+export class SubscriptionsService {
+    constructor(
+        @InjectRepository(Subscription)
+        private readonly subscriptions: Repository<Subscription>
+    ) { }
+
+    async subscribe(email: string): Promise<Subscription> {
+        const subscription = this.subscriptions.create({ email });
+
+        return this.subscriptions.save(subscription);
+    }
+}
